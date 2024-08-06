d4ee27571755cfd416e5607aa4dcb6683e1459fd
Manish kumar <manojgeetparmar@gmail.com>
2024-08-06 15:40:05 +0530

    New build

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import React from 'react'
-import { Header, Section1, Section2, Section3, Section4, Section5, Section6, Footer, Section7 } from "./Sections"
-import { Stack } from '@mui/material'
-import { useScreenWidth } from './context/useScreenWidth'
+import React from "react";
+import {
+  Header,
+  Section1,
+  Section2,
+  Section3,
+  Section4,
+  Section5,
+  Section6,
+  Footer,
+  Section7,
+} from "./Sections";
+import { Stack } from "@mui/material";
+import { useScreenWidth } from "./context/useScreenWidth";
+import whatsapp from "./assets/WhatsAppButtonGreenSmall.svg";
 
 const App = () => {
-  const screenWidth = useScreenWidth()
+  const screenWidth = useScreenWidth();
   return (
-    <Stack sx={{backgroundColor: "whiteSmoke"}}>
+    <Stack sx={{ backgroundColor: "whiteSmoke" }}>
       {/* Header */}
-      <Header screenWidth={screenWidth}/>
+      <Header screenWidth={screenWidth} />
       {/* section 1 - For all your Gadgets */}
-      <Section1 screenWidth={screenWidth}/>
+      <Section1 screenWidth={screenWidth} />
       {/* section 2 - We bring the best services to You */}
-      <Section2 screenWidth={screenWidth}/>
+      <Section2 screenWidth={screenWidth} />
       {/* section 3 - We are best at Laptop home Service! */}
-      <Section3 screenWidth={screenWidth}/>
+      <Section3 screenWidth={screenWidth} />
       {/* section 7 - We are best at Laptop home Service! */}
-      <Section7 screenWidth={screenWidth}/>
+      <Section7 screenWidth={screenWidth} />
       {/* section 4 - Don’t know whats the problem? */}
-      <Section4 screenWidth={screenWidth}/>
+      <Section4 screenWidth={screenWidth} />
       {/* section 5 - Read what our clients say */}
-      <Section5 screenWidth={screenWidth}/>
+      <Section5 screenWidth={screenWidth} />
       {/* section 6 - Schedule A Free Home Visit */}
-      <Section6 screenWidth={screenWidth}/>
+      <Section6 screenWidth={screenWidth} />
       {/* Footer */}
-      <Footer screenWidth={screenWidth}/>
+      <Footer screenWidth={screenWidth} />
+      <a
+        style={{
+          position: "fixed",
+          bottom: "10px",
+          right: "10px",
+        }}
+        aria-label="Chat on WhatsApp"
+        href="https://wa.me/+918586885226"
+      >
+        <img alt="Chat on WhatsApp" src={whatsapp} />
+      </a>
     </Stack>
-  )
-}
+  );
+};
 
-export default App
+export default App;
